faf836aec1672bbf5059dde9856012feee11c3bc
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-03-27 12:47:12 +0400

    Initial check-in
    maven-archetype-quickstart Eclipse project
    jzmq dependency in pom.xml
    Pyrolite submodule

---
+++ src/main/java/com/samsung/veles/mastodon/App.java
@@ -0,0 +1,13 @@
+package com.samsung.veles.mastodon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
