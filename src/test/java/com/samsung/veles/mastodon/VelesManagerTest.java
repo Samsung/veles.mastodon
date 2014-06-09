3a38eb47344d6eb2dda694d7529715a46c2c1814
Vadim Markovtsev <v.markovtsev@samsung.com>
2014-06-09 17:58:02 +0400

    Covered updateZmqEndpoints() with tests
    Change-Id: Id568d87e2ce7b429eb5b1d74940fe143f52ee62d

--- src/test/java/com/samsung/veles/mastodon/VelesManagerTest.java
+++ src/test/java/com/samsung/veles/mastodon/VelesManagerTest.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import com.alibaba.fastjson.JSON;
 
 /**
  * Unit test for VelesManager.
@@ -37,8 +39,9 @@ public class VelesManagerTest extends TestCase {
    *
    * @throws UnsupportedEncodingException
    */
-  public void testApp() throws UnsupportedEncodingException {
-    JSON.parse("{\"fd8e0fc6-b015-4245-922d-950dea3ac198\": "
+  @SuppressWarnings("unchecked")
+  public void testUpdateZmqEndpoints() throws UnsupportedEncodingException {
+    byte[] json = ("{\"fd8e0fc6-b015-4245-922d-950dea3ac198\": "
         + "{\"mid\": \"9d88104eccced2100a4a3ee851f7e8b0-c8600098e75f\", "
         + "\"pid\": 25766, \"host\": \"markovtsevu64\", \"power\": 100, "
         + "\"id\": \"fd8e0fc6-b015-4245-922d-950dea3ac198\", \"state\": "
@@ -46,7 +49,49 @@ public class VelesManagerTest extends TestCase {
         + "{\"inproc\": [\"connect\", "
         + "\"inproc://veles-zmqloader-ZeroMQLoader\"], \"tcp\": "
         + "[\"connect\", \"tcp://*:52937\"], \"ipc\": [\"connect\", "
-        + "\"ipc:///tmp/veles-ipc-zmqloader-dnioqryd\"]}}, null]}}");
+        + "\"ipc:///tmp/veles-ipc-zmqloader-dnioqryd\"]}}, null]}}").getBytes();
+    Method method = null;
+    try {
+      method = VelesManager.class.getDeclaredMethod("updateZmqEndpoints",
+          byte[].class);
+    } catch (NoSuchMethodException | SecurityException e1) {
+      fail(e1.getMessage());
+    }
+    method.setAccessible(true);
+    try {
+      method.invoke(VelesManager.instance(), json);
+    } catch (IllegalAccessException | IllegalArgumentException |
+        InvocationTargetException e2) {
+      fail(e2.getMessage());
+    }
+    Field field = null;
+    try {
+      field = VelesManager.class.getDeclaredField("_endpoints");
+    } catch (NoSuchFieldException | SecurityException e3) {
+      fail(e3.getMessage());
+    }
+    field.setAccessible(true);
+    Map<String, Map<String, String>> endpoints = null;
+    try {
+      endpoints =
+          (Map<String, Map<String, String>>) field.get(VelesManager.instance());
+    } catch (IllegalArgumentException | IllegalAccessException e4) {
+      fail(e4.getMessage());
+    }
+    assertEquals(1, endpoints.size());
+    assertTrue(endpoints.containsKey("fd8e0fc6-b015-4245-922d-950dea3ac198"));
+    Map<String, String> val = endpoints.get(
+        "fd8e0fc6-b015-4245-922d-950dea3ac198");
+    assertEquals(3, val.size());
+    assertTrue(val.containsKey("inproc"));
+    assertTrue(val.containsKey("ipc"));
+    assertTrue(val.containsKey("tcp"));
+    String ep = val.get("inproc");
+    assertEquals("inproc://veles-zmqloader-ZeroMQLoader", ep);
+    ep = val.get("ipc");
+    assertEquals("ipc:///tmp/veles-ipc-zmqloader-dnioqryd", ep);
+    ep = val.get("tcp");
+    assertEquals("tcp://markovtsevu64:52937", ep);
   }
 
   public void testChecksum() throws IOException {
@@ -54,13 +99,13 @@ public class VelesManagerTest extends TestCase {
     PrintWriter writer = new PrintWriter(tmp.getAbsolutePath(), "ASCII");
     writer.println("test text to check VelesManager.checksum()");
     writer.close();
-    tmp.delete();
     String cs = null;
     try {
       cs = VelesManager.checksum(tmp.getAbsolutePath());
     } catch (NoSuchAlgorithmException e) {
       fail(e.getMessage());
     }
+    tmp.delete();
     assertEquals("2fbb51403bc48c145de6febf39193e42c34ef846", cs);
   }
 }
